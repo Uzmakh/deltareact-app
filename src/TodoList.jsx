175e2a63d0637e97b3d2e01b6699e8960de15b83
Uzma Khan <urbazoha@gmail.com>
2025-05-09 20:25:59 +0500

    Added the feature of updating All-items and One-item

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -10,17 +10,49 @@ export default function TodoList() {
     setTodos([...todos, { task: newTodo, id: uuidv4() }]);
     setNewTodo("");
   };
+
   let updateTodoValue = (event) => {
     // console.log(event.target.value);
     setNewTodo(event.target.value);
   };
+
   let deleteTodo = (id) => {
     console.log(id);
-    // ? This filter method creates a copy of the array,consisting on those elements whose id not matches the selected item's id.
-      let copy = todos.filter((todo) => todo.id != id);
-    console.log(copy);
+    // ? This filter method creates a copy of the array, consisting on those elements whose id not matches the selected item's id.
+    //   let copy = todos.filter((todo) => todo.id != id);
+    // console.log(copy);
     // setTodos(todos.filter((todo) => todo.id != id));
     setTodos((prevTodos) => todos.filter(prevTodos.id != id));
+  };
+
+  let upperCaseAllTodo = () => {
+    setTodos((todos) =>
+      todos.map((todo) => {
+        // console.log(todo);
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      })
+    );
+    // console.log(newArr);
+  };
+
+  let upperCaseOne = (id) => {
+    // console.log('One to uppercase');
+    setTodos((todos) =>
+      todos.map((todo) => {
+        // console.log(todo);
+        if (todo.id == id) {
+          return {
+            ...todo,
+            task: todo.task.toUpperCase(),
+          };
+        } else {
+          return todo;
+        }
+      })
+    );
   };
   return (
     <>
@@ -42,9 +74,11 @@ export default function TodoList() {
             <span>{todo.task}</span>
             &nbsp;
             <button onClick={() => deleteTodo(todo.id)}>delete</button>
+            <button onClick={() => upperCaseOne(todo.id)}>Uppercase One</button>
           </li>
         ))}
       </ul>
+      <button onClick={upperCaseAllTodo}>Uppercase All</button>
     </>
   );
 }
